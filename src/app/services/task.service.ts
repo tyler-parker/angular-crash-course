d530097c83cca5dbabca475ab30231fd4afcc253
Tyler Parker <tparker42d2@gmail.com>
2021-12-20 15:15:16 -0700

    created a task form component. moving on to functionality of form

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
-import {Observable, of} from 'rxjs'
+import {Observable} from 'rxjs'
 import {Task} from '../Task'
-import {TASKS} from '../mock-tasks'
+
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -19,5 +25,10 @@ export class TaskService {
   deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`
     return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task, httpOptions)
   }
 }
