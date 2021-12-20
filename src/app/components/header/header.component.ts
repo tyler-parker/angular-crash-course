af37ea0526db042c4a3f68993d3abbd2da1be9df
Tyler Parker <tparker42d2@gmail.com>
2021-12-20 16:19:29 -0700

    created a form display toggle in the ui service to manage the toggle. implemented service in header and add-task componenets

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UiService } from 'src/app/services/ui.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask: boolean = false
+  subscription!: Subscription
 
-  constructor() { }
+  constructor(private uiService: UiService) {
+    this.subscription = this.uiService.onToggle().subscribe(value => this.showAddTask = value)
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask() {
-    console.log('toggle');
+    this.uiService.toggleAddTask()
   }
 
 }
